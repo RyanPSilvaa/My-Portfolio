c983edcaaf40fb138cad97dcdfca9f9dd6215112
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2025-01-03 16:15:40 -0300

    Atualizei as imagens e informações

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelector('.section');
-let navLinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('.section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let top = window.scrollY;  // Movi para fora do loop para otimizar
+
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id+']').classlist.add('active')
-            })
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active'); // Remove a classe 'active' de todos os links
+            });
+            // Aqui a busca é pelo link com o 'href' que corresponde ao id da seção
+            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
         }
-    })
-}
+    });
+};
 
 menuIcon.onclick = () => {
-menuIcon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
-}
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
